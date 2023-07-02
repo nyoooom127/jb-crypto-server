480cd7a07d343c72c469332a94ebc56d1d595b48
nyoooom127 <tamar.peles@gmail.com>
2023-07-02 16:50:53 +0300

    graph

--- controllers/user.js
+++ controllers/user.js
@@ -19,17 +19,52 @@ const dashboard = async (req, res, next) => {
         const userSymbols = await userSymbol.findByUserId({
             userId: req.user.id
         });
-
-        const promises = userSymbols.map((userSymbol) => SymbolValue.findOne({ symbol: userSymbol.symbol }).sort({ createdAt: -1 }).limit(1));
-        const symbolValues = await Promise.all(promises);
+        const today = new Date();
+        const lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
         const colors = randomColor({
             count: userSymbols.length,
             luminosity: 'bright'
         });
 
+        const promises = userSymbols.map((userSymbol) => SymbolValue.findOne({ symbol: userSymbol.symbol }).sort({ createdAt: -1 }).limit(1));
+        const timePromises = userSymbols.map((userSymbol) => SymbolValue.find({ symbol: userSymbol.symbol, createdAt: { $gte: lastMonth, $lte: today } }).sort({ createdAt: -1 }).limit(500));
+        const symbolValues = await Promise.all(promises);
+        const timeValues = await Promise.all(timePromises);
+        // const timeSymbolValues = new Map(
+        //     timeValues.map((timeValue, index) => {
+        //         return [timeValue[0].symbol, ({
+        //             label: timeValue[0].symbol,
+        //             data: timeValue.map(t => t.value).reverse(),
+        //             borderColor: colors[index],
+        //             fill: false
+        //         })];
+        //     }),
+        // );
+
+        const timeSymbolValues = //timeValues;
+            timeValues.map((timeValue, index) => ({
+                    label: timeValue[0].symbol,
+                    data: timeValue.map(t => t.value).reverse(),
+                    borderColor: colors[index],
+                    fill: false
+                })
+        );
+
+//         .map((timeValue, index) => ({
+//             label: timeValue[0].symbol,
+//             data: timeValue.map(t => t.value).reverse(),
+//             borderColor: colors[index],
+//             fill: false
+//         })
+// );
+
+        console.log(timeSymbolValues)
+
+
         res.render('user/dashboard', {
             userSymbols,
             symbolValues,
+            timeSymbolValues,
             colors
         });
     } catch (err) {
